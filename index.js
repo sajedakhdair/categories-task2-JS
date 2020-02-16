2a0107f35dc5937caeb2ddc383e1ba68711101a2
sajedakhdair <sajeda.khdair@gmail.com>
2020-02-16 10:24:24 +0200

    add addCategory and  validateItem functions

--- index.js
+++ index.js
@@ -3,5 +3,36 @@ var arrString = fs.readFileSync("./categories.json", "utf-8");
 var arr = JSON.parse(arrString);
 main();
 function main() {
-
+  addCategory({
+    name: "sajeda",
+    desc: "hi",
+    parentId: 3
+  });
+  addCategory({
+    name: "sajeda",
+    desc: "hi",
+    parentId: 3
+  });
 }
+
+function addCategory(item) {
+  let id = validateItem(item);
+  if (id) {
+    newitem = { id, ...item };
+    arr.push(newitem);
+    arr;
+    console.log(arr);
+  }
+}
+function validateItem(item) {
+  let myname = item.name;
+  let pid = item.parentId;
+  const values = Object.values(arr[arr.length - 1]);
+  const id = values[0] + 1;
+  const arrayofname = arr.filter(object => object.name === myname);
+  if (arrayofname.length == 0) {
+    //////////////////
+    if (pid == null || typeof pid === "number") return id;
+  } else return false;
+}
+arr;

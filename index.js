9a896ad0d04d9b22988edce242ac85bda555be21
sajedakhdair <sajeda.khdair@gmail.com>
2020-02-16 13:40:59 +0200

    add deleteCategoryById function

--- index.js
+++ index.js
@@ -9,10 +9,42 @@ function main() {
     parentId: 3
   });
   addCategory({
-    name: "sajeda",
+    name: "sama",
     desc: "hi",
     parentId: 3
   });
+  addCategory({
+    name: "anal",
+    desc: "hi",
+    parentId: 2
+  });
+  addCategory({
+    name: "anal2",
+    desc: "hi",
+    parentId: 2
+  });
+  addCategory({
+    name: "anal2",
+    desc: "hi",
+    parentId: 2
+  });
+
+  //    console.log(arr);
+  //    console.log("*******************************************************************");
+//   deleteCategoryById(11);
+//   deleteCategoryById(7);
+  //deleteCategoryById([10,11,12,13,14]);
+ deleteCategoryById([9,20,30,12,2]);
+
+
+  // console.log(arr);
+  //console.log("*******************************************************************8");
+  // deleteCategoryById([20,7]);
+  //   console.log(arr);
+  // console.log("*******************************************************************8");
+  // deleteCategoryById([8,9,10,11,12,13]);
+  //    console.log(arr);
+  //console.log("*******************************************************************8");
 }
 
 function addCategory(item) {
@@ -21,7 +53,6 @@ function addCategory(item) {
     newitem = { id, ...item };
     arr.push(newitem);
     arr;
-    console.log(arr);
   }
 }
 function validateItem(item) {
@@ -36,3 +67,35 @@ function validateItem(item) {
   } else return false;
 }
 arr;
+function deleteCategoryById(ids) {
+    let ids_exist=[];
+    var result;
+  if (!Array.isArray(ids)) {
+    arr = arr.filter(object => object.id !== ids);
+  } else {
+      for(let j =0 ;j<ids.length;j++)
+      {
+      result =arr.filter(object=>object.id===ids[j] )
+     if (result.length!=0){
+     ids_exist.push(result);
+     }
+    }
+    console.log(ids_exist);
+
+      if(ids.length==ids_exist.length)
+       {
+           for (let i=0;i<ids.length;i++)
+          {
+          arr = arr.filter(object => object.id !== ids[i]);
+          }
+       }
+       else 
+       { 
+        for(let i=0; i< ids_exist.length;i++ ) 
+        {
+            console.log("this" +ids_exist[i].id+"is not exist"); 
+        }          
+       }
+  }
+}
+console.log(arr);
